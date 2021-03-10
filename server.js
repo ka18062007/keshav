7ad2ec99a8635df0ace12ddd59f48f6713091586
Keshav Agarwal <ka18062007@gmail.com>
2021-03-10 09:07:59 +0530

    production build

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 require('./routes/web')(app)
+
 app.use((req, res) => {
     res.status(404).render('errors/404')
 })
